9db420525f8cca77a265e4dfe6116c616e44849e
horribleinternet <cyberblah@gmail.com>
2025-10-13 03:21:41 +0900

    add indexing object

---
+++ cli/inverted_index.py
@@ -0,0 +1,40 @@
+from movie_data import tokenize
+from pickle import dump
+import os
+
+class InvertedIndex:
+    def __init__(self):
+        self.index = dict()
+        self.docmap = dict()
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize(text)
+        for token in tokens:
+            if token not in self.index:
+                self.index[token] = {int(doc_id),}
+            else:
+                self.index[token].add(int(doc_id))
+
+    def get_document(self, term):
+        term = term.lower()
+        if term in self.index:
+            out = list(self.index[term])
+            out.sort()
+            return out
+        return []
+
+    def build(self, data):
+        for movie in data:
+            desc = f"{movie["title"]} {movie["description"]}"
+            self.__add_document(movie["id"], desc)
+            self.docmap[int(movie["id"])] = movie
+
+    def save(self):
+        try:
+            os.stat("cache")
+        except:
+            os.mkdir("cache")
+        with open("cache/index.pkl", "wb") as f:
+            dump(self.index, f)
+        with open("cache/docmap.pkl", "wb") as f:
+            dump(self.docmap, f)
