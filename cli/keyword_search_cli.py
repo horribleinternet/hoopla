7e8d0f63a2cb7301a0d35f88f46cc4f6ce6c7cfe
horribleinternet <cyberblah@gmail.com>
2025-11-03 21:20:29 +0900

    add parsing term searching

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -12,6 +12,13 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    tf_parser = subparsers.add_parser("tf", help="The thre frequency of a term in a movie description")
+    tf_parser.add_argument("movie_id", type=int, help="Id number of movie")
+    tf_parser.add_argument("term", type=str, help="Term to count")
+
+    idf_parser = subparsers.add_parser("idf", help="Find the inverse document frequency of a term")
+    idf_parser.add_argument("term", type=str, help="Term to find")
+                           
     args = parser.parse_args()
 
     match args.command:
@@ -32,13 +39,28 @@ def main() -> None:
                     break
             for id in out[:5]:
                 print(id, indexer.docmap[id]["title"])
-           
         case "build":
             print(f"Building database")
             data = read_data()
             indexer = InvertedIndex()
             indexer.build(data)
             indexer.save()
+        case "tf":
+            try:
+                indexer = InvertedIndex()
+                indexer.load()
+                print(indexer.get_tf(str(args.movie_id), args.term))
+            except Exception as e:
+                print(e)
+            return
+        case "idf":
+            try:
+                indexer = InvertedIndex()
+                indexer.load()
+                idf = indexer.get_idf(args.term)
+                print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+            except Exception as e:
+                print(e)
         case _:
             parser.print_help()
 
